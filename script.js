ba31a5599e9e8a5657c5513c0a6adfb872133b52
Cristian10-code <cristiansarm@gmail.com>
2025-07-06 16:39:48 -0500

    Update js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ const slideContents = {
                     <div class="laptop">
                         <div class="laptop-screen">
                             <video class="laptop-video" controls muted>
-                                <source src="ACM_EC_SheetRevisor.mp4" type="video/mp4">
+                                <source src="Videos/ACM_EC_SheetRevisor.mp4" type="video/mp4">
                                 Your browser does not support the video element.
                             </video>
                         </div>
@@ -183,7 +183,7 @@ const slideContents = {
                     <div class="laptop">
                         <div class="laptop-screen">
                             <video class="laptop-video" controls muted>
-                                <source src="ACM_EC_WEB_BEP_Revisor.mp4" type="video/mp4">
+                                <source src="Videos/ACM_EC_WEB_BEP_Revisor.mp4" type="video/mp4">
                                 Your browser does not support the video element.
                             </video>
                         </div>
